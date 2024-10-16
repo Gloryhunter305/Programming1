b13ca819543178a53c1a661f60fa7a8c112d10be
Gloryhunter305 <mikylejosh@gmail.com>
2024-10-16 13:51:42 -0400

    Ideas? Not yet anyways

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -46,7 +46,7 @@ public class CoinScript : MonoBehaviour
         }
         
         
-        //transform.position = new Vector2(Random.Range(-10, 10), Random.Range(-4.5f,4.5f));
+        //transform.position = new Vector2(Random.Range(-10, 10), Random.Range(-4.5f,4.5f);
     }
 
     private void OnDrawGizmos()
